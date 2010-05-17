8a797066c8d8fe3069df5443904bc226e78321b0
Eddie Cianci <defeated2k4@gmail.com>
2010-05-16 21:28:14 -0400

    iteration D2

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,10 @@
 class Product < ActiveRecord::Base
   default_scope order('title')
   
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+  
   validates :title, :description, :image_url, :presence => true
   validates :price, :numericality => { :greater_than_or_equal_to => 0.01 }
   validates :title, :uniqueness => true
@@ -9,5 +13,17 @@ class Product < ActiveRecord::Base
     :with	=> %r{\.(gif|jpg|jpeg|png)$}i,
     :message => 'must be a URL for GIF, JPG or PNG image.'
   }
+
+private
+  
+  # ensure that there are no line items referencing this product
+  def ensure_not_referenced_by_any_line_item
+    if line_items.count.zero?
+      return true
+    else
+      errors[:base] << "Line Items present"
+      return false
+    end
+  end
   
 end
